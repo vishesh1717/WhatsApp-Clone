d34d317ef3778be384c53fcc8a152f2118e56301
kakashi <69907823+vishesh1717@users.noreply.github.com>
2021-05-03 15:36:32 +0530

    first

---
+++ src/components/Login.js
@@ -0,0 +1,40 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import { auth, provider } from '../firebase'
+import { actionTypes } from '../reducer'
+import { useStateValue } from '../StateProvider'
+import './Login.css'
+
+
+const Login = () => {
+    const [{ }, dispatch] = useStateValue()
+
+
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+            .then(result => {
+                dispatch({
+                    type: actionTypes.SET_USER,
+                    user: result.user,
+                })
+            }
+            ).catch((error) => alert(error.message))
+    }
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="" />
+                <div className="login__text">
+                    Login In to WhatsApp
+                </div>
+
+                <Button onClick={signIn}>
+                    Sign in with Google
+                    </Button>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login
